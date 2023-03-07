c9cb938d8f7435efb7407cea0134ca899c904f62
01BIJAY <108603525+01BIJAY@users.noreply.github.com>
2023-03-07 20:29:23 +0545

    Initial commit

---
+++ src/components/MenuItems.js
@@ -0,0 +1,31 @@
+export const MenuItems = [
+  {
+    title: "Home",
+    url: "/",
+    cName: "nav-link",
+    icon: "fa-solid fa-solid fa-house-user"
+  },
+  {
+    title: "Search",
+    url: "/search",
+    cName: "nav-link",
+    icon: "fa-solid fa-magnifying-glass"
+  },
+  {
+    title: "Request a call",
+    url: "/call",
+    cName: "nav-link",
+    icon: "fa-solid fa-regular fa-phone-volume"
+  },
+  {
+    title: "Language",
+    url: "/language",
+    cName: "nav-link"
+  },
+  {
+    title: "Sign Up",
+    url: "/signup",
+    cName: "nav-link-mobile",
+    icon: "fa-solid fa-regular fa-user"
+  }
+];
